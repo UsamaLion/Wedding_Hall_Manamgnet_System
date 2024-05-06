be454be93de8af6593f6bbb7ea983fabd146f914
M.Usama <ulion269@gmail.com>
2024-05-06 17:31:42 +0500

    testing

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'header.php'; ?>
 
 <h1>Welcome to Wedding Hall Management System</h1>
+<h2>Hello did it go to website</h2>
 
 <p>This system helps you manage bookings, income, and expenses for your wedding hall.</p>
 
